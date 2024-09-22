fbe9bddc362cb08728439c5f838d5ac2831d9230
Ak202411270 <kendelalex10@gmail.com>
2024-09-22 14:17:38 -0300

    Creating a Python Study Exposure Beginner.

---
+++ olá.py
@@ -0,0 +1,26 @@
+print('Olá,mundo')
+print('7'+'4')
+print('Olá',5)
+nome='Guanabará'
+idade='25'
+peso='76'
+print(nome,idade,peso)
+
+nomecompleto=input('Qual é o seu nome?')
+age=input('Qual é a sua idade?' )
+pesocompletoa=input('Qual é o seu peso?')
+print(nomecompleto,age,pesocompletoa)
+
+name=input('What is your name?')
+print(('Olá'),name,('Prazer em te conhecer'))
+
+dia=input('dia:')
+mês=input('mês:')
+ano=input('ano')
+
+print(('Você nasceu no dia'),dia,mês,ano,('.Correto?'))
+
+primeironúmero=input('Primeiro número')
+segundonúmero=input('Segundo número')
+
+print(('A soma é:'),primeironúmero+segundonúmero)
